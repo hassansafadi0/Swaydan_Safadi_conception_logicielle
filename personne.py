305d79db1e1626e582bcb81c0b466c708f83291f
hassansafadi0 <hassansafadi200@example.com>
2026-04-01 17:25:40 +0200

    architecture global

--- personne.py
+++
@@ -1,8 +0,0 @@
-class personne:
-    def __init__(self, nom, prenom, age):
-        self.nom = nom
-        self.prenom = prenom
-        self.age = age
-
-    def afficher_info(self):
-        print(f"Nom: {self.nom}, Prénom: {self.prenom}, Âge: {self.age}")
